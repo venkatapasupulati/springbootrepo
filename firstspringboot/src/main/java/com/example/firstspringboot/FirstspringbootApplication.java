78d889c82912bda2ecfe8e3a86a48cc983fe55c5
venkatapasupulati <89496703+venkatapasupulati@users.noreply.github.com>
2022-02-12 11:50:56 -0500

    Update FirstspringbootApplication.java

--- firstspringboot/src/main/java/com/example/firstspringboot/FirstspringbootApplication.java
+++ firstspringboot/src/main/java/com/example/firstspringboot/FirstspringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstspringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("my comment");
 		SpringApplication.run(FirstspringbootApplication.class, args);
 	}
 
